d650cd5dddc347b4d27f7892e385e5e1cc9a87c7
pedrodemmer <pedrohenriquedemmer@hotmail.com>
2024-10-25 19:52:50 -0300

    route

--- src/app/api/add-vaga/route.js
+++ src/app/api/add-vaga/route.js
@@ -1,18 +1,20 @@
-import { sql } from '@vercel/postgres';
-import { NextResponse } from 'next/server';
- 
-export async function GET(request: Request) {
+const { sql } = require('@vercel/postgres');
+const { NextResponse } = require('next/server');
+
+async function GET(request) {
   const { searchParams } = new URL(request.url);
-  const petName = searchParams.get('petName');
-  const ownerName = searchParams.get('ownerName');
- 
+  const modeloVeiculo = searchParams.get('modeloVeiculo');
+  const placa = searchParams.get('placa');
+
   try {
-    if (!petName || !ownerName) throw new Error('Pet and owner names required');
-    await sql`INSERT INTO Pets (Name, Owner) VALUES (${petName}, ${ownerName});`;
+    if (!modeloVeiculo || !placa) throw new Error('Modelo do veiculo e placa requiridos!');
+    await sql`INSERT INTO Veiculo (Modelo, Placa) VALUES (${Modelo}, ${Placa});`;
   } catch (error) {
-    return NextResponse.json({ error }, { status: 500 });
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
- 
-  const pets = await sql`SELECT * FROM Pets;`;
-  return NextResponse.json({ pets }, { status: 200 });
+
+  const veiculos = await sql`SELECT * FROM Veiculo;`;
+  return NextResponse.json({ veiculos }, { status: 200 });
 }
+
+module.exports = { GET };
