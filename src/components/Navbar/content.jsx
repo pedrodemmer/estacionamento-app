37cb3bc54240439b99f947760f1caf6e89bb74a4
pedrodemmer <pedrohenriquedemmer@hotmail.com>
2024-11-04 20:32:46 -0300

    Feat: Adicão Responsividade no Navbar

--- src/components/Navbar/content.jsx
+++ src/components/Navbar/content.jsx
@@ -1,11 +1,35 @@
+"use client"
+import { useState } from 'react';
 import Button from '@/components/Button/content';
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <header className="flex justify-between items-center bg-gray-800 text-white w-full h-auto p-4">
             <div className="text-2xl font-bold">Logo</div>
 
-            <nav className="flex space-x-4">
+            <button 
+                className="sm:hidden text-white focus:outline-none"
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                <svg 
+                    className="w-6 h-6" 
+                    fill="none" 
+                    stroke="currentColor" 
+                    viewBox="0 0 24 24" 
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path 
+                        strokeLinecap="round" 
+                        strokeLinejoin="round" 
+                        strokeWidth="2" 
+                        d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"} 
+                    />
+                </svg>
+            </button>
+
+            <nav className={`flex-col sm:flex-row sm:flex space-x-4 items-center ${isOpen ? 'flex' : 'hidden'} sm:block`}>
                 <Button label="Histórico" />
                 <Button label="Adicionar Veículo" />
                 <Button label="Meus Veículos" />
